7097aec83155571707633baf52ce53dee5549927
Polako912 <michal.polak37@gmail.com>
2019-06-02 15:51:35 +0200

    join and leave event

--- src/home/LandingPage.js
+++ src/home/LandingPage.js
@@ -12,12 +12,53 @@ import React, { Component } from 'react'
 import '@trendmicro/react-sidenav/dist/react-sidenav.css'
 import axios from 'axios';
 import Auth from "../services/Auth.js";
+import SearchField from "react-search-field";
 
-const Row = ({ name, date, place }) =>
+const handleJoinEvent = ({id}) => {
+  const requestOptions =
+    axios.post('https://letsmeet.azurewebsites.net/api/meetings/' + id + '/join', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Auth.getToken()}`,
+      }
+    })
+  return fetch('https://letsmeet.azurewebsites.net/api/meetings/' + id + '/join', requestOptions)
+  .then(function (response) {
+    if(response.ok) {
+      alert("Successfully joined event");
+    }
+    else {
+      alert("You have already joined that event")
+    }
+  })
+}
+
+const handleLeaveEvent = ({id}) => {
+  const requestOptions =
+    axios.post('https://letsmeet.azurewebsites.net/api/meetings/' + id + '/leave', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${Auth.getToken()}`,
+      }
+    })
+  return fetch('https://letsmeet.azurewebsites.net/api/meetings/' + id + '/leave', requestOptions)
+  .then(function (response) {
+    if(response.ok) {
+      alert("Successfully left event");
+    }
+    else {
+      alert("You have already left that event")
+    }
+  })
+}
+
+const Row = ({ name, date, place, id }) =>
   <tr>
     <td>{name}</td>
     <td>{date}</td>
     <td>{place}</td>
+    <td><Button onClick={ () => handleJoinEvent({id})}>Join</Button></td>
+    <td><Button noClick={ () => handleLeaveEvent({id})}>Leave</Button></td>
   </tr>
 
 class Event extends React.Component {
@@ -31,7 +72,7 @@ class Event extends React.Component {
         'place': '',
         'size': '',
       },
-    }
+    };
   }
 
   componentDidMount () {
@@ -82,6 +123,11 @@ export default class LandingPage extends Component {
     return (
       <React.Fragment>
         <NavbarPage></NavbarPage>
+        <SearchField
+          placeholder="Search..."
+          searchText="This is initial search text"
+          classNames="test-class"
+        />
         <Event></Event>
       </React.Fragment>
 
